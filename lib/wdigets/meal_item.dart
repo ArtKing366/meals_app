ac628b11a9225c34631fc2c498dfa34ec94229e8
Artem <artemgolovachov1256789@gmail.com>
2024-06-29 19:02:54 +0300

    update meal

--- lib/wdigets/meal_item.dart
+++ lib/wdigets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/wdigets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -9,6 +10,15 @@ class MealItem extends StatelessWidget {
   });
 
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
+
+    String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +66,27 @@ class MealItem extends StatelessWidget {
                       height: 12,
                     ),
                     Row(
-                      children: [],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.timer,
+                          label: "${meal.duration} min",
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                         const SizedBox(
+                          width: 12,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.money_sharp,
+                          label: affordabilityText,
+                        ),
+                      ],
                     )
                   ],
                 ),
