0f4ddaf65bed95445a101621a325b5e511e1d337
Artem <artemgolovachov1256789@gmail.com>
2024-07-05 01:25:48 +0300

    clear

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:meals_app/models/meal.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meals_app/providers/favorites_providers.dart';
